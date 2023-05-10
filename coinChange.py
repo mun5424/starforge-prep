79e883cdf45be901b6c43e1d4c5249c11e808efe
mun5424 <mun5424@gmail.com>
2023-05-10 11:09:11 -0700

    coins sum to a certain target true/false

--- coinChange.py
+++ coinChange.py
@@ -34,7 +34,7 @@ def coinChangeWays(coins, n):
 # find minimum number of coins to reach a target n given a set of coins
 # take 1. Greedy Approach 
 def greedyMinCoins(coins, target):
-    # greedy approach, this doesnt actually give you the min number of coins sometimes 
+    # greedy approach, this solution may work for some but not all. 
     coins.sort()
     count = 0 
     for i in range(len(coins), -1, -1):
@@ -45,7 +45,7 @@ def greedyMinCoins(coins, target):
     return count 
 
 
-
+# find minimum number of coins to reach a target n given a set of coins
 def minCoins(coins, target): 
     # dynamic programming approach
     dp = [sys.maxsize] * (target+1) 
@@ -64,4 +64,36 @@ print(minCoins([25, 10, 5], 30)) # 2
 print(minCoins([9,6,5,1], 13)) # 3 
 print(minCoins([1,3,5,7], 18)) # 4 
 
+ 
 
+
+
+# return true if the given set of coins can sum up to a target
+def coinSumsToTarget(coins, target):
+    if target == 0:
+        return True
+    
+    # dynamic programming approach to check if a given subset of coins sum up to a target and caching this 
+    # ex: [1,2,3] and target = 6 
+    # [3] with target 3 is a subproblem
+    # [1,2] with target 3 is also a subproblem 
+    # we notice that once we have a subarray that sums up to a target, we can re-use this value 
+    # so then, the solutions is 
+    # dp[i + c] = dp[i] 
+    # this way we check every number possible up to this sum 
+    dp = [False] * (target + 1) 
+    dp[0] = True
+    for i in range(0, target+1):
+        for c in coins: 
+           if dp[i] and i + c <= target:
+               dp[i + c] = True
+            
+    
+    return dp[target]
+
+
+
+print(coinSumsToTarget([25, 10, 5], 30)) # True
+
+print(coinSumsToTarget([7, 9], 8)) # False
+print(coinSumsToTarget([1,3,5,7], 18)) # True
