23dfcef69452aa34519c0fafbed2c58f04436e8b
mun5424 <mun5424@gmail.com>
2023-05-10 11:46:19 -0700

    combination sum, each cand can be used more than once

--- combinationSum.py
+++ combinationSum.py
@@ -1,12 +1,30 @@
 # given a set of numbers check if it sums to a certain number 
 
 from typing import List
+# given a set of numbers, check if it sums to a certain number 
+# each item can be used infinite numbers of times     
+
+def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    res = [] 
+    def dfs(path, idx, target):
+        if target == 0:
+            res.append(path) 
+        if target < 0:
+            return 
+        
+        for i in range(idx, len(candidates)): 
+            dfs(path + [candidates[i]], i, target - candidates[i])
+    
+    dfs([], 0, target) 
+    return res 
 
 
-#
+
+
+
+# given a set of numbers, check if it sums to a certain number 
+# each item can only be used once
 # runtime is O(2^n))
-
-
 def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
     res = [] 
     candidates.sort()
