774fdbbe3ca0dd045ba7fac3524e662136ad377e
mun5424 <mun5424@gmail.com>
2023-05-16 00:02:38 -0700

    permutations with duplicate elements allowed

--- permutations.py
+++ permutations.py
@@ -28,7 +28,27 @@ def permute(nums: List[int]) -> List[List[int]]:
                 visited.remove(i)
     
     bt([], 0)
+    return res
 
 permute([1,2,3]) 
 # [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
 
+
+
+
+# Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+def permuteUnique(nums: List[int]) -> List[List[int]]:
+    res = []
+    strSet = set()
+
+    def bt(nums, current, currStr, res):
+        if not nums and currStr not in strSet:
+            res.append(current)
+            strSet.add(currStr)
+        for i in range(len(nums)):
+            bt(nums[:i] + nums[i+1:], current  + [nums[i]], currStr + str(nums[i]), res)
+            
+    bt(nums, [], "", res)
+    return res
+
+print(permuteUnique([1,1,2]))
