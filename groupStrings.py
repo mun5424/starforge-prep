5fa5f8d4877372bb8e90f86dca1692d92a80472a
mun5424 <mun5424@gmail.com>
2023-05-18 12:39:43 -0700

    group strings explanation and test cases

--- groupStrings.py
+++ groupStrings.py
@@ -8,20 +8,26 @@ For example, we can keep shifting "abc" to form the sequence: "abc" -> "bcd" -> 
 Given an array of strings strings, group all strings[i] that belong to the same shifting sequence. You may return the answer in any order.
 """
 
-from collections import defaultdict
 from typing import List
+from collections import defaultdict
 
 
 # need to figure out the association between letters of each word and hash these.
 # fortunately, we can keep a list of differences between indices of ascii values in the format () i.e. (1,1,1) -> abc, cde
 # we can use a map to store them. Essentially hashing the values
-def groupStrings(strings: List[str]) -> List[List[str]]:
-    groups = defaultdict(List) 
-    for s in strings: 
-        pattern = () 
-        for i in range(1, len(s)): 
-            pattern += pattern + ((ord(s[i]) - ord(s[i-1]) + 26) % 26,)
-        groups[pattern].append(s)
-    
-    return groups.values() 
+class Solution: 
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        groups = {}
+        for s in strings: 
+            pattern = ()
+            for i in range(1, len(s)): 
+                pattern += pattern + ((ord(s[i]) - ord(s[i-1]) + 26) % 26,)
+            if pattern not in groups:
+                groups[pattern] = []
+            groups[pattern].append(s)
+        
+        return groups.values() 
 
+sol = Solution()
+
+print(sol.groupStrings(["abc","cde","aef", "bfg"]))
