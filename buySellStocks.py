90dd255aa7d5a6532c7a40461334cbd18a504be0
mun5424 <mun5424@gmail.com>
2023-05-19 11:38:05 -0700

    buy sell stocks, sellable same-day

--- buySellStocks.py
+++ buySellStocks.py
@@ -27,3 +27,23 @@ def maxProfit(prices: List[int]) -> int:
 
 print(maxProfit([7,1,5,3,6,4])) # 5
 print(maxProfit([7,6,4,3,1])) # 0
+
+
+"""
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+"""
+
+
+def maxProfit(self, prices: List[int]) -> int:
+    minsofar = 1000000
+    totalmax = 0
+    for i in range(1, len(prices)):
+        if prices[i] > prices[i-1]:
+            totalmax += prices[i] - prices[i-1]
+    
+    return totalmax
+        
