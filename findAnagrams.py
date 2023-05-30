4ad6a5d48987482595913f202971bf25a9896756
mun5424 <mun5424@gmail.com>
2023-05-30 13:31:22 -0700

    find anagrams of string comments

--- findAnagrams.py
+++ findAnagrams.py
@@ -1,3 +1,8 @@
+"""
+Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+"""
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         if len(p) > len(s):
