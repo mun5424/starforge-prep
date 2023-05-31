656974b0de35fd608f1b413dab3d07d3579193ad
mun5424 <mun5424@gmail.com>
2023-05-31 11:38:42 -0700

    lowest common ancestor

--- getPathFromAToBTree.py
+++ getPathFromAToBTree.py
@@ -14,3 +14,32 @@ from node 1 to node 11 would be:
 [up, up, right, left]
 
 """
+
+# approach is finding the LCA - then finding the path from node A to that LCA, path from node B to that LCA, add them together. 
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def lca(root, p, q):
+            if root == p or root == q:
+                return root
+            left = None
+            right = None 
+            if root.left:
+                left = lca(root.left, p, q) 
+            if root.right:
+                right = lca(root.right, p, q) 
+                
+            if left and right:
+                return root
+            
+            return left or right 
+        
+        return lca(root, p, q)
