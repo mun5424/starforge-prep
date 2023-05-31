289636b22eba05617ccad629cae441c65d125f89
mun5424 <mun5424@gmail.com>
2023-05-31 11:20:48 -0700

    get path from A to B on a binary tree

---
+++ getPathFromAToBTree.py
@@ -0,0 +1,16 @@
+"""
+given a binary tree, get path from node A to node B.
+for example: 
+
+							4
+						/      \
+					3            6
+				/    \         /   \
+			  1     9       11    4
+			 /              /  \
+			13            19    12
+
+from node 1 to node 11 would be:
+[up, up, right, left]
+
+"""
