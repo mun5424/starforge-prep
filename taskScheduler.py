475014602854637bcc01289b1d8c2db18a26b84e
mun5424 <mun5424@gmail.com>
2023-06-04 18:35:00 -0700

    task scheduler comments

--- taskScheduler.py
+++ taskScheduler.py
@@ -1,3 +1,13 @@
+"""
+Given a characters array tasks, representing the tasks a CPU needs to do, where each letter represents a different task. Tasks could be done in any order. Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
+
+However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
+
+Return the least number of units of times that the CPU will take to finish all the given tasks.
+"""
+
+# we want to sort the tasks by the frequencies in increasing order and pop from the end to get the task that occurs the most to least
+
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         # frequencies of the tasks
