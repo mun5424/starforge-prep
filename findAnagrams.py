7f1622a13ca25515c253f01614a54a8bd6f08f33
mun5424 <mun5424@gmail.com>
2023-07-04 09:10:55 -0700

    test cases for findAnagrams

--- findAnagrams.py
+++ findAnagrams.py
@@ -3,6 +3,9 @@ Given two strings s and p, return an array of all the start indices of p's anagr
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 """
+from typing import List
+
+
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         if len(p) > len(s):
@@ -18,4 +21,8 @@ class Solution:
                 
         return res
                 
-            
+
+sol = Solution()
+print(sol.findAnagrams('abc','cba'))
+print(sol.findAnagrams('dfe','sdf'))
+print(sol.findAnagrams('ac','df'))
