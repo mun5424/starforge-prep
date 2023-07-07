a2474e4561d3df7c33f7256df6a3001e90f16b54
mun5424 <mun5424@gmail.com>
2023-07-06 22:51:09 -0700

    find leaves test case

--- findLeavesOfBinaryTree.py
+++ findLeavesOfBinaryTree.py
@@ -1,4 +1,5 @@
 # collect all the leaf nodes until the tree is empty.
+import collections
 from typing import Collection, List
 
 class TreeNode:
@@ -9,7 +10,7 @@ class TreeNode:
 
 class Solution:
     def findLeaves(self, root: 'TreeNode') -> List[str]:
-        res = Collection.defaultdict(list) 
+        res = collections.defaultdict(list) 
         
         def dfs(node, layer):
             if not node:
@@ -26,3 +27,13 @@ class Solution:
         return res.values() 
 
 
+
+sol = Solution() 
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.left = TreeNode(6)
+
+print(sol.findLeaves(root)) # [[4, 5, 6], [2, 3], [1]]
