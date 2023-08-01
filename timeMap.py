883a820925250206c210b98271a0b8996cd7b501
mun5424 <mun5424@gmail.com>
2023-08-01 12:24:53 -0700

    time map

---
+++ timeMap.py
@@ -0,0 +1,32 @@
+import collections
+import bisect
+
+"""
+Design a time-based key-value data structure that can store multiple values for the same key at different time stamps and retrieve the key's value at a certain timestamp.
+
+Implement the TimeMap class:
+
+    TimeMap() Initializes the object of the data structure.
+    void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
+    String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns "".
+
+"""
+
+
+# solution is to create two dictionaries - one for key-value pair and the other for key-timestamp pair. 
+# do a binary search on the timestamp of the list so that it returns according index of the value 
+
+class TimeMap:
+    def __init__(self):
+        self.meta = collections.defaultdict(list)
+        self.data = collections.defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.meta[key].append(timestamp)
+        self.data[key].append(value)
+
+    def get(self, key: str, timestamp: int) -> str:
+        idx = bisect.bisect(self.meta[key], timestamp)
+        if idx == 0:
+            return ''
+        return self.data[key][idx - 1]
